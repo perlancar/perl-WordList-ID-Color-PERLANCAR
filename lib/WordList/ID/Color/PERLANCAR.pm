c88a05d6e315df6877641fcda0e20fc19ad28b91
perlancar (on pc-home) <perlancar@gmail.com>
2020-06-07 09:28:47 +0700

    Initial commit

---
+++ lib/WordList/ID/Color/PERLANCAR.pm
@@ -0,0 +1,63 @@
+package WordList::ID::Color::PERLANCAR;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use WordList;
+our @ISA = qw(WordList);
+
+# STATS
+
+1;
+# ABSTRACT: List of color names in Indonesian
+
+=head1 DESCRIPTION
+
+Suitable for playing hangman.
+
+Some variations included, e.g. coklat & cokelat, abu & abu-abu, merah marun &
+marun, orange & jingga.
+
+Not all "WARNA muda/tua/gelap/terang" included, e.g. "kuning tua" or "ungu muda"
+are not included. But "merah muda" is included.
+
+
+=head1 INTERNAL NOTES
+
+krem - beige
+
+=head1 SEE ALSO
+
+=cut
+
+__DATA__
+abu
+abu-abu
+biru
+biru dongker
+cokelas
+coklat
+emas
+gading
+hijau
+hitam
+jingga
+krem
+lembayung
+magenta
+marun
+merah
+merah jambu
+merah marun
+merah muda
+nila
+oranye
+perak
+perunggu
+putih
+sian
+ungu
+violet
+zaitun
